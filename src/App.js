1f0b85adc10ac3cf20c211461151fb41bb61c8a8
guillem-koa <guillem.cobos@koabiotech.com>
2024-08-19 14:23:20 +0200

    some pruning

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, PathogenLine, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, Reporting, ColonyCounting, Sequencing } from './pages';
+import { Orders, Line, PathogenLine, Reporting, ColonyCounting, Sequencing } from './pages';
 import './App.css';
 
 import { useStateContext } from './contexts/ContextProvider';
