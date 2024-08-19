f8c0637370242c971bb7eddfdaa047b4a94af2ed
guillem-koa <guillem.cobos@koabiotech.com>
2024-08-19 16:38:20 +0200

    index corrections

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,9 +3,9 @@ export { default as Orders } from './Orders';
 export { default as Line } from './Charts/Line';
 export { default as PathogenLine } from './Charts/PathogenLine';
 
-export { default as Pie } from './Charts/Pie';
+/*export { default as Pie } from './Charts/Pie';
 export { default as Pyramid } from './Charts/Pyramid';
-export { default as Stacked } from './Charts/Stacked';
+export { default as Stacked } from './Charts/Stacked'; */
 
 export { default as Reporting } from './Reporting';
 export { default as ColonyCounting } from './ColonyCounting';
